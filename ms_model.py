1412e5e500b6e869178db44e4ef82682624efd1c
elliott.long <elliott.long@wsu.edu>
2020-04-17 12:38:13 -0700

    View Fixed, Scalable
    View is working how it should, and can make puzzle bigger
    Small glitch when selecting certain tiles, to be fixed next

--- ms_model.py
+++ ms_model.py
@@ -5,16 +5,13 @@ import random
 
 class msModel:
     def __init__(self):
-        self.newGame()
+        self.newGame(1)
 
-    def newGame(self):
-        # 1. Initialize your game grid(s)
-        # 2. Add an appropriate number of bombs
-        # 3. Figure out adjacent bomb counts
+    def newGame(self, bombs):
         clicked = 0
-        cols = 10
-        rows = 10
-        bombs = 10
+        cols = bombs
+        rows = bombs
+        bombs = bombs
         grid = [[0] * (rows) for i in range(cols)]
         self.puzzle = grid
         self.clicked = clicked
@@ -22,8 +19,8 @@ class msModel:
         bombCount = rows
         bomb = '*'
         for x in range(0, rows):
-            y = random.randrange(10)
-            z = random.randrange(10)
+            y = random.randrange(rows)
+            z = random.randrange(cols)
             if grid[y][z] != bomb:
                 grid[y][z] = bomb
             else:
@@ -39,30 +36,56 @@ class msModel:
                                 grid[x][y] += 1
                             if grid[1][1] == '*':
                                 grid[x][y] += 1
+                        elif y == cols - 1:
+                            for q in range(x, x + 2):
+                                for z in range(y - 1, y + 1):
+                                    if grid[q][z] == '*':
+                                        grid[x][y] += 1
                         else:
-                            for q in range(x, x+2):
-                                for z in range(y-1, y+1):
+                            for q in range(x, x + 2):
+                                for z in range(y - 1, y + 2):
+                                    if grid[q][z] == '*':
+                                        grid[x][y] += 1
+                    elif x == rows - 1:
+                        if y == 0:          # if in first col
+                            if grid[rows - 2][0] == '*':
+                                grid[x][y] += 1
+                            if grid[rows - 2][1] == '*':
+                                grid[x][y] += 1
+                            if grid[rows - 1][1] == '*':
+                                grid[x][y] += 1
+                        elif y == cols - 1:
+                            if grid[rows - 2][cols - 1] == '*':
+                                grid[x][y] += 1
+                            if grid[rows - 2][cols - 2] == '*':
+                                grid[x][y] += 1
+                            if grid[rows - 1][cols - 2] == '*':
+                                grid[x][y] += 1
+                        else:
+                            for q in range(x - 1, x + 1):
+                                for z in range(y - 1, y + 2):
                                     if grid[q][z] == '*':
                                         grid[x][y] += 1
                     else:
-                        for q in range(x-1, x + 1):
-                            if y != 0:
+                        for q in range(x - 1, x + 2):
+                            if y != 0 and y != cols - 1:
+                                for z in range(y - 1, y + 2):
+                                    if grid[q][z] == '*':
+                                        grid[x][y] += 1
+                            elif y == cols - 1:
                                 for z in range(y - 1, y + 1):
                                     if grid[q][z] == '*':
                                         grid[x][y] += 1
                             else:
-                                for z in range(y, y + 1):
+                                for z in range(y, y + 2):
                                     if grid[q][z] == '*':
                                         grid[x][y] += 1
-        for x in range(0, 10):
+        for x in range(0, bombs):
             print(grid[x])
 
     def reveal(self, row, col):
         self.clicked += 1
         return self.puzzle[row][col]
-
-    #def getSquare(self):
-		# returns the current state of an indicated square (revealed or not, adj. bomb count)
 
     #def getMoveCount(self):
         # returns the number of moves made
--- ms_view.py
+++ ms_view.py
@@ -19,40 +19,51 @@ class MSWindow(QMainWindow):
         box = QHBoxLayout()
         self.puzzle1 = QLabel("Hi")
         self.puzzle1.setFrameStyle(QFrame.StyledPanel | QFrame.Sunken)
-        self.puzzle1.setAlignment(Qt.AlignLeft)
-        self.puzzle1.setGeometry(QRect(100, 100, 100, 100))
+        self.puzzle1.setAlignment(Qt.AlignCenter)
         box.addWidget(self.puzzle1)
         self.puzzle2 = QLabel("Ho")
-        self.puzzle2.setGeometry(QRect(100, 100, 100, 100))
         self.puzzle2.setFrameStyle(QFrame.StyledPanel | QFrame.Sunken)
-        self.puzzle2.setAlignment(Qt.AlignRight)
-        self.puzzle2.setGeometry(QRect(100, 100, 100, 100))
+        self.puzzle2.setAlignment(Qt.AlignCenter)
         box.addWidget(self.puzzle2)
         layout = QVBoxLayout()
-        box.addLayout(layout)
+        layout.addLayout(box)
         grid = QGridLayout()
         self.buttons = []
-        for i in range(100):
+        j, pressed = QInputDialog.getInt(self, "Size Selector", "Number of Bombs:", 10, 5, 20, 1)
+        for i in range(j * j):
             button = QPushButton()
             button.clicked.connect(self.buttonClicked)
             self.buttons.append(button)
-            button.setText('hi')
-            # button.setText(button.label)
-            row = i // 10 # integer division by # of columns to get the row
-            col = i % 10  # remainder when divided by # of cols gives us a column
+            button.setText('')
+            button.setFixedWidth(35)
+            row = i // j # integer division by # of columns to get the row
+            col = i % j  # remainder when divided by # of cols gives us a column
             grid.addWidget(self.buttons[i], row, col) # note we set the location
             button.setProperty("myRow", row)
             button.setProperty("myCol", col)
+        grid.setHorizontalSpacing(0)
+        grid.setVerticalSpacing(0)
         layout.addLayout(grid)
-        widget.setLayout(box)
+        widget.setLayout(layout)
         self.model = msModel()
-        self.newGame()
+        self.newGame(j)
+        menu = self.menuBar().addMenu("&Game")
+        newAct = QAction("&New", self, shortcut=QKeySequence.New, triggered=self.newGame)
+        menu.addAction(newAct)
+        menu.addSeparator()
+        quitAct = QAction("E&xit", self, shortcut=QKeySequence.Quit, triggered=self.close)
+        menu.addAction(quitAct)
 
-    def newGame(self):
+    def newGame(self, i):
         print("Start a new game!")
         for button in self.buttons:
             button.setEnabled(True)
-        self.model.newGame()
+            button.setText('')
+        self.model.newGame(i)
+        x = self.model.bombs
+        y = str(x)
+        self.puzzle1.setText(y)
+        self.puzzle1.repaint()
 
     def buttonClicked(self):
         # sender() tells us who caused the action to take place
@@ -60,25 +71,64 @@ class MSWindow(QMainWindow):
         row = clicked.property("myRow")
         col = clicked.property("myCol")
         x = self.model.reveal(row, col)
-        x = str(x)
-        print(x)
-        clicked.setText(x)
+        y = str(x)
+        print(y)
+        clicked.setText(y)
         clicked.setEnabled(False)
+        clicked.repaint()
         print(self.model.clicked)
+        if x == 0:
+            if row != 0 and row != self.model.bombs - 1:
+                if col != 0 and col != self.model.bombs - 1:
+                    for i in range(row - 1, row + 2):
+                        for j in range(col - 1, col + 2):
+                            self.buttons[i*10 + j].click()
+                elif col == 0:
+                    for i in range(row - 1, row + 2):
+                        for j in range(col, col + 2):
+                            self.buttons[i * 10 + j].click()
+                elif col == self.model.bombs - 1:
+                    for i in range(row - 1, row + 2):
+                        for j in range(col - 1, col + 1):
+                            self.buttons[i * 10 + j].click()
+            elif row == 0:
+                if col == 0:
+                    for i in range(row, row + 2):
+                        for j in range(col, col + 2):
+                            self.buttons[i * 10 + j].click()
+                elif col == self.model.bombs - 1:
+                    for i in range(row, row + 2):
+                        for j in range(col - 1, col + 1):
+                            self.buttons[i * 10 + j].click()
+                else:
+                    for i in range(row, row + 2):
+                        for j in range(col - 1, col + 2):
+                            self.buttons[i * 10 + j].click()
+            elif row == self.model.bombs - 1:
+                if col == 0:
+                    for i in range(row - 1, row + 1):
+                        for j in range(col, col + 2):
+                            self.buttons[i * 10 + j].click()
+                elif col == self.model.bombs - 1:
+                    for i in range(row - 1, row + 1):
+                        for j in range(col - 1, col + 1):
+                            self.buttons[i * 10 + j].click()
+                else:
+                    for i in range(row - 1, row + 1):
+                        for j in range(col - 1, col + 2):
+                            self.buttons[i * 10 + j].click()
         state = self.model.getState(x, self.model.clicked)
         if state == -1:
+            self.end(state, self.model.bombs)
+        if state == 1:
+            self.end(state, self.model.bombs)
+
+    def end(self, state, j):                          # ends game, works
+        for i in range(j * j):
+            self.buttons[i].click()
+        if state == -1:
             self.puzzle1.setText("BOOM")
-            self.end()
+            self.puzzle1.repaint()
         if state == 1:
             self.puzzle1.setText("WINNER")
-            self.end()
-
-    def end(self):                          # ends game, works
-        for i in range(100):
-            self.buttons[i].click()
-
-    def hello(self):
-        print("bob")
-
-
-# whsat about this
+            self.puzzle1.repaint()
